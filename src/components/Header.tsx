e0e54e132cb1bd4d3493af402fef39bbef2a1211
Nick Coffey <nicholasjcoffey@gmail.com>
2023-06-07 11:46:28 -0500

    minor mobile nav tweaks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,18 +6,18 @@ import { ROUTES } from '@/consts'
 import profilePic from '../../public/profile.jpeg'
 
 export function Header() {
-  const [showNavigation, setShowNavigation] = useState(false)
+  const [showMobileNavigation, setShowMobileNavigation] = useState(false)
 
-  const openModal = () => setShowNavigation(true)
-  const closeModal = () => setShowNavigation(false)
+  const openModal = () => setShowMobileNavigation(true)
+  const closeModal = () => setShowMobileNavigation(false)
 
   return (
     <header className='flex items-center justify-between h-10 mb-12'>
       <Image src={profilePic} alt='Profile' className='rounded-full h-full w-10' />
-      <FaBars className='text-2xl' onClick={openModal} />
-      {showNavigation && (
+      {!showMobileNavigation && <FaBars className='text-2xl' onClick={openModal} />}
+      {showMobileNavigation && (
         <nav
-          className='flex flex-col gap-10 items-center justify-center bg-black text-white fixed top-0 left-0 z-50 h-full w-full'
+          className='flex flex-col gap-10 items-center justify-center bg-black opacity-90 text-white fixed top-0 left-0 z-50 h-full w-full'
           onClick={closeModal}
         >
           <FaTimes className='fixed top-6 right-3 text-2xl' />
