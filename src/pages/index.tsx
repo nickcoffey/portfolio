2995f1815ec466753de42fcd48f8118647f44e38
Nick Coffey <nicholasjcoffey@gmail.com>
2023-06-07 17:08:20 -0500

    more lint fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import { CONTACTS } from '@/consts'
 export default function Home() {
   return (
     <main className='flex flex-col gap-12'>
-      <h1 className='text-4xl font-bold leading-sn'>Hi, my name is Nick Coffey and I'm a fullstack developer.</h1>
+      <h1 className='text-4xl font-bold'>Hi, my name is Nick Coffey and I&#39;m a fullstack developer.</h1>
       <p className='leading-loose'>
-        I've been working as a web developer professionally since 2017 and love the constant learning and innovation
+        I&#39;ve been working as a web developer professionally since 2017 and love the constant learning and innovation
         that comes with building on the web. This blog is where I like to share new ideas, technologies, and discoveries
         I come across in my personal and professional projects.
       </p>
