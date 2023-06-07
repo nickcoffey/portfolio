f0ff34f7bc0b9ef470dccbdf8864c4cbe0cdb049
Nick Coffey <nicholasjcoffey@gmail.com>
2023-06-06 23:15:48 -0500

    removed dark colors

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" className='antialiased'>
       <Head />
-      <body className='py-4 px-3 bg-slate-100 dark:bg-slate-900 dark:text-white transition-colors duration-150'>
+      <body className='py-4 px-3'>
         <Main />
         <NextScript />
       </body>
