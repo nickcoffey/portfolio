c736affbe8ca6ae0f5905b3cf898a32c68c7eb10
Nick Coffey <nicholasjcoffey@gmail.com>
2023-08-09 14:23:46 -0500

    custom 404 & 500 pages

---
+++ src/pages/500.tsx
@@ -0,0 +1,8 @@
+export default function Custom500() {
+  return (
+    <main className='flex flex-col gap-6 items-center py-48'>
+      <h1 className='text-4xl font-bold'>Whoops!</h1>
+      <p className='text-xl'>Something went wrong</p>
+    </main>
+  )
+}
